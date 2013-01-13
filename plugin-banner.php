22ba586fda429b75b6260fc90b7169602efa167f
Mike <mike@trenvo.com>
2013-01-13 17:00:09 +0200

    Rewrite

--- plugin-banner.php
+++ plugin-banner.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH'))
  *
  * @since 0.1
  */
-define('PLUGIN_BANNER_VERSION', '0.1');
+define('PLUGIN_BANNER_VERSION', '1.0');
 
 /**
  * PATHs and URLs
@@ -27,75 +27,35 @@ define('PLUGIN_BANNER_VERSION', '0.1');
 define('PLUGIN_BANNER_DIR', plugin_dir_path(__FILE__));
 define('PLUGIN_BANNER_URL', plugin_dir_url(__FILE__));
 
-if (!class_exists('WP_PluginBanner')) :
+if ( !function_exists( 'plugin_banner_shortcode' ) ) :
 
-    class WP_PluginBanner    {
+    add_shortcode('plugin-banner', 'plugin_banner_shortcode' );
+    function plugin_banner_shortcode( $atts ) {
+        $atts = extract(shortcode_atts(array(
+            'slug' => '',
+            'name' => '',
+            'img'  => ''
+        ), $atts));
 
-        private $plugin_name = '';
-        private $plugin_img  = '';
-
-        /**
-         * Creates an instance of the WP_PluginBanner class
-         *
-         * @return WP_PluginBanner object
-         * @since 0.1
-         * @static
-        */
-        public static function &init( $atts, $content = 0 ) {
-            $instance = new WP_PluginBanner( $atts );
+        if ( empty ( $name ) ) {
+            if ( empty ( $slug ) ) return;
+            $name = $slug;
         }
 
-        /**
-         * Constructor
-         *
-         * @since 0.1
-         */
-        public function __construct( $atts ) {
-            $atts = extract(shortcode_atts(array(
-                'slug' => '',
-                'name' => '',
-                'img'  => ''
-            ), $atts));
-
-            if ( empty ( $name ) && empty ( $slug ) ) return;
-
-            if ( ! empty ( $img ) ) {
-                $this->plugin_img = $img;
-            } elseif ( ! empty ( $slug ) )
-                $this->plugin_img = "http://s-plugins.wordpress.org/$slug/assets/banner-772x250.png";
-            else return;
-
-            $this->plugin_name = ( ! empty ( $name ) ) ? $name : $slug;
-
-            $this->enqueue_style();
-            $this->display();
+        if ( empty ( $img ) ) {
+            if ( empty ( $slug ) ) return;
+            $img = "http://s-plugins.wordpress.org/$slug/assets/banner-772x250.png";
         }
 
-            /**
-             * PHP4
-             *
-             * @since 0.1
-             */
-            public function WP_PluginBanner() {
-                $this->__construct();
-            }
+        wp_enqueue_style( 'plugin_banner', PLUGIN_BANNER_URL . 'plugin-banner.css', null, 1.0 );
 
-        private function enqueue_style() {
-            wp_enqueue_style( 'plugin_banner', PLUGIN_BANNER_URL . 'plugin-banner.css', null, 1.0 );
-        }
-
-        private function display() {
-            ?>
-            <div class="plugin-banner-sc">
-                <div class="vignette"></div>
-                <img src="<?php echo $this->plugin_img ?>">
-                <h2 itemprop="name"><?php echo $this->plugin_name ?></h2>
-            </div>
-            <?php
-        }
-
+        return "
+            <div class='plugin-banner-sc'>
+               <div class='vignette'></div>
+               <img src='$img'>
+               <h2 itemprop='name'>$name</h2>
+           </div>";
 
     }
 
-    add_shortcode('plugin-banner', array ( WP_PluginBanner, 'init' ) );
 endif;
